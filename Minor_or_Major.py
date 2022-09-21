08c549f46913a06a2fe0de369bc482907dcfd259
Kamal Vikhram <113769096+kamal-vikhram@users.noreply.github.com>
2022-09-21 12:14:28 +0530

    To Determine Whether the user is Minor or Major
    We all know that if a person crosses the age 18 he is considered as a major and the same is applicable for this program if the user enter his age above than 18 the output is displayed as major and if its below 18 its displayed as minor.

---
+++ Minor_or_Major.py
@@ -0,0 +1,6 @@
+#Program to define the given age is minor or major
+age=(int(input("What is Your Age \n"))) #Gets the input age from the user
+if age > 18:  
+    print("Major") 
+else:
+    print("Minor")
